3362bbff6720fba3bef5c2e4e5f59526a4d811c2
Jafet <jafet@XLP.local>
2022-04-11 09:31:55 -0500

    [REF] Elimina import innecesario

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, Fragment, useContext } from 'react';
+import { useEffect, useState, useContext } from 'react';
 
 import AuthContext from '../context/AuthContext';
 import useApi from '../hooks/useApi';
